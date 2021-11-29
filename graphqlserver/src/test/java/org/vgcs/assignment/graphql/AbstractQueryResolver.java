988643b3fd35fb12166ede687f6ae59c0b1d4482
Andrei Cazaceanu <andrei.cazaceanu@gmail.com>
2021-11-29 22:53:24 +0100

    Uses WireMock templating for testing purposes. Hides the stubbing.

--- graphqlserver/src/test/java/org/vgcs/assignment/graphql/AbstractQueryResolver.java
+++ graphqlserver/src/test/java/org/vgcs/assignment/graphql/AbstractQueryResolver.java
@@ -1,9 +1,7 @@
 package org.vgcs.assignment.graphql;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import com.graphql.spring.boot.test.GraphQLTestTemplate;
 import io.micrometer.core.instrument.util.IOUtils;
 import org.junit.jupiter.api.AfterAll;
@@ -15,22 +13,14 @@ import org.springframework.core.io.ClassPathResource;
 import org.vgcs.assignment.persistance.VehicleInfoRepo;
 import org.vgcs.assignment.persistance.VehicleRepo;
 import org.vgcs.assignment.persistance.VehicleServicesRepo;
-import org.vgcs.assignment.restservice.VehicleInfoService;
-import org.vgcs.assignment.restservice.VehicleService;
-import org.vgcs.assignment.restservice.VehicleServicesService;
-import org.vgcs.assignment.restservice.dto.*;
 
 import java.io.IOException;
-import java.util.List;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-
-@WireMockTest
 public class AbstractQueryResolver {
     protected static final String GRAPHQL_QUERY_REQUEST_PATH = "graphql/request/%s.graphql";
     protected static final String GRAPHQL_QUERY_RESPONSE_PATH = "graphql/response/%s.json";
     private static ObjectMapper objectMapper;
-    private static  WireMockServer wireMockServer;
+    private static WireMockServer wireMockServer;
 
     @Autowired
     protected GraphQLTestTemplate graphQLTestTemplate;
@@ -63,73 +53,7 @@ public class AbstractQueryResolver {
         vehicleServicesRepo.deleteAll();
     }
 
-    void wireMockResponse(String value, int responseCode, String uri) throws JsonProcessingException {
-        wireMockServer.stubFor(get(urlEqualTo(String.format("/vehicle%s", uri)))
-                .withHeader("Content-Type", equalTo("application/json"))
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(value)
-                        .withStatus(responseCode)));
-
-    }
-
     String read(String location) throws IOException {
         return IOUtils.toString(new ClassPathResource(location).getInputStream());
     }
-
-
-    void mockVehicleList() throws JsonProcessingException {
-        VehicleResponseDTO body = new VehicleResponseDTO(
-                List.of(
-                        new VehicleDTO("bd45a676-0d0e-48b4-9693-e8196eb7fcbf", "big truck"),
-                        new VehicleDTO("2337d25f-8917-4e26-920f-ddbe9ba063d6", "small truck")
-                ));
-        wireMockResponse(objectMapper.writeValueAsString(body), 200, "/list/");
-    }
-
-    void mockVehicleInfo(String id, int statusCode) throws JsonProcessingException {
-        VehicleInfoResponseDTO body = null;
-        String errorMessage = "{Cannot fetch resource}";
-        switch (id) {
-            case ("bd45a676-0d0e-48b4-9693-e8196eb7fcbf"):
-                body = new VehicleInfoResponseDTO("+4678625847", "OK", "Thor's fleet", "Volvo Construction Equipment", "Japan", "000543", "VCE");
-                break;
-            case ("2337d25f-8917-4e26-920f-ddbe9ba063d6"):
-                body = new VehicleInfoResponseDTO("+4635489215", "DANGER", "Thor's fleet", "Volvo Trucks", "Mongolia", "954634", "VT");
-                break;
-            default:
-                return;
-        }
-
-        if(statusCode == 400 || statusCode == 401 || statusCode == 403 || statusCode == 500 || body == null)
-            wireMockResponse(errorMessage, statusCode, "/info?id=" + id);
-        else
-            wireMockResponse(objectMapper.writeValueAsString(body), statusCode, "/info?id="+id);
-    }
-
-    void mockVehicleServices(String id, int statusCode) throws JsonProcessingException {
-        VehicleServicesResponseDTO body = null;
-        String errorMessage = "{Cannot fetch resource}";
-
-        switch (id) {
-            case ("bd45a676-0d0e-48b4-9693-e8196eb7fcbf"):
-                body = new VehicleServicesResponseDTO("ACTIVE", List.of(
-                        new ServiceDTO("GPS", "ACTIVE", "2019-01-01T09:23:05+01:00"),
-                        new ServiceDTO("FuelMeasurement", "DEACTIVATED", "2019-01-01T09:23:05+01:00")));
-                break;
-            case ("2337d25f-8917-4e26-920f-ddbe9ba063d6"):
-                body = new VehicleServicesResponseDTO("ACTIVE", List.of(
-                        new ServiceDTO("GPS", "ACTIVE", "2019-01-01T09:23:05+01:00"),
-                        new ServiceDTO("FuelMeasurement", "ACTIVATED", "2019-01-01T09:23:05+01:00")));
-                break;
-            default:
-                return;
-        }
-
-        if(statusCode == 400 || statusCode == 401 || statusCode == 403 || statusCode == 500 || body == null)
-            wireMockResponse(errorMessage, statusCode, "/services?id="+id);
-        else
-            wireMockResponse(objectMapper.writeValueAsString(body), statusCode, "/services?id="+id);
-    }
-
 }
--- graphqlserver/src/test/java/org/vgcs/assignment/graphql/QueryByIdTest.java
+++ graphqlserver/src/test/java/org/vgcs/assignment/graphql/QueryByIdTest.java
@@ -1,7 +1,6 @@
 package org.vgcs.assignment.graphql;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
-import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import com.graphql.spring.boot.test.GraphQLResponse;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
@@ -14,17 +13,14 @@ import java.io.IOException;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = AssignmentApplication.class)
 @TestPropertySource(locations = "classpath:application-test.properties")
+@WireMockTest
 class QueryByIdTest extends AbstractQueryResolver {
 
 
     @Test
     void resolve_vehicles_OK_test() throws IOException, JSONException {
-        var queryPath = "queryById/request";
+        var queryPath = "queryById/request_exists";
         var responsePath = "queryById/response_exists";
-
-        mockVehicleList();
-        mockVehicleServices("bd45a676-0d0e-48b4-9693-e8196eb7fcbf", 200);
-        mockVehicleInfo("bd45a676-0d0e-48b4-9693-e8196eb7fcbf", 200);
 
         GraphQLResponse response = graphQLTestTemplate.postForResource(String.format(GRAPHQL_QUERY_REQUEST_PATH, queryPath));
         var expectedResponseBody = read(String.format(GRAPHQL_QUERY_RESPONSE_PATH, responsePath));
@@ -38,20 +34,16 @@ class QueryByIdTest extends AbstractQueryResolver {
 
     @Test
     void resolve_vehicles_error_test() throws IOException, JSONException {
-        var requestPath = "queryById/request";
+        var requestPath = "queryById/request_error";
         var responsePath = "queryById/response_error";
-
-        mockVehicleList();
-        mockVehicleServices("bd45a676-0d0e-48b4-9693-e8196eb7fcbf", 401);
-        mockVehicleInfo("bd45a676-0d0e-48b4-9693-e8196eb7fcbf", 401);
 
         GraphQLResponse response = graphQLTestTemplate.postForResource(String.format(GRAPHQL_QUERY_REQUEST_PATH, requestPath));
         var expectedResponseBody = read(String.format(GRAPHQL_QUERY_RESPONSE_PATH, responsePath));
 
         assert (response.isOk());
         JSONAssert.assertEquals(expectedResponseBody, response.getRawResponse().getBody(), true);
-        assert (vehicleRepo.findById("bd45a676-0d0e-48b4-9693-e8196eb7fcbf").isPresent());
-        assert (vehicleInfoRepo.findById("bd45a676-0d0e-48b4-9693-e8196eb7fcbf").isEmpty());
-        assert (vehicleServicesRepo.findById("bd45a676-0d0e-48b4-9693-e8196eb7fcbf").isEmpty());
+        assert (vehicleRepo.findById("f7c8402d-98ca-4728-9c3e-0805c3ecffbb").isPresent());
+        assert (vehicleInfoRepo.findById("f7c8402d-98ca-4728-9c3e-0805c3ecffbb").isEmpty());
+        assert (vehicleServicesRepo.findById("f7c8402d-98ca-4728-9c3e-0805c3ecffbb").isEmpty());
     }
 }
